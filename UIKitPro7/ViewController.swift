4aca20116793998f30020d84409b5955a2db7831
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-26 15:25:30 -0700

    Part1: Add Score Label

--- UIKitPro7/ViewController.swift
+++ UIKitPro7/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    // پاسخ فعلی
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
